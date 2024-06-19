17f1bf3383e188e5a10f87ab45d330116b90a740
RiverMatt <47513427+RiverMatt@users.noreply.github.com>
2024-06-19 14:28:02 -0600

    Update boot_to_boot-efi.sh

--- boot_to_boot-efi.sh
+++ boot_to_boot-efi.sh
@@ -1,29 +1,30 @@
-echo "Clearing /boot/..."
+echo "\n\nClearing /boot/..."
 rm -rf /boot/*
 
-echo "Unmounting /boot/..."
+echo "\n\nUnmounting /boot/..."
 umount /boot
 
-echo "Installing linux kernel and generating images..."
+echo "\n\nInstalling linux kernel and generating images..."
 pacman -Sy linux
 
-echo "Mounting /boot/efi..."
+echo "\n\nMounting /boot/efi..."
 mkdir /boot/efi
 
-echo "Editing /etc/fstab..."
+echo "\n\nEditing /etc/fstab..."
 sed -i 's/\/boot/\/boot\/efi/g' /etc/fstab
 
-echo "Reloading systemctl daemons to use the updated /etc/fstab..."
+echo "\n\nReloading systemctl daemons to use the updated /etc/fstab..."
 systemctl daemon-reload
 
-echo "Finding path for ESP device..."
+echo "\n\nFinding path for ESP device..."
 ESP=$(awk '/\/boot/ {print substr(prev, 3); exit} {prev=$0}' /etc/fstab)
+echo "\nFound ESP device as $ESP"
 
-echo "Mounting ESP device..."
+echo "\n\nMounting ESP device..."
 mount $ESP /boot/efi
 
-echo "Installing grub..."
+echo "\n\nInstalling grub..."
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
 
-echo "Creating grub config..."
+echo "\n\nCreating grub config..."
 grub-mkconfig -o /boot/grub/grub.cfg
