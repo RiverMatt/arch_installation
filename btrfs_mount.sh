98596e1157817cb2d346552a293901888794e97c
RiverMatt <47513427+RiverMatt@users.noreply.github.com>
2024-06-19 00:31:28 -0600

    Update btrfs_mount.sh

--- btrfs_mount.sh
+++ btrfs_mount.sh
@@ -5,7 +5,7 @@ mount -o subvol=@home /dev/nvme0n1p2 /mnt/home
 mount /dev/nvme0n1p1 /mnt/boot
 mount -o subvol=@.snapshots /dev/nvme0n1p2 /mnt/.snapshots
 mkdir -p /mnt/timeshift-btrfs/snapshots/
-mount -o subvol=timeshift-btrfs/snapshots /dev/nvme0n1p2 /mnt/timeshift-btrfs/snapshots
+mount timeshift-btrfs/snapshots /dev/nvme0n1p2 /mnt/timeshift-btrfs/snapshots
 
 mount --bind /dev /mnt/dev
 mount --bind /proc /mnt/proc
