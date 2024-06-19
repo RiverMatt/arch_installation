a13e01b91440fdf2e9da6da83dae440fc743321e
RiverMatt <47513427+RiverMatt@users.noreply.github.com>
2024-06-19 00:30:18 -0600

    Create btrfs_mount.sh

---
+++ btrfs_mount.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mount -o subvol=@ /dev/nvme0n1p2 /mnt
+mount -o subvol=@home /dev/nvme0n1p2 /mnt/home
+mount /dev/nvme0n1p1 /mnt/boot
+mount -o subvol=@.snapshots /dev/nvme0n1p2 /mnt/.snapshots
+mkdir -p /mnt/timeshift-btrfs/snapshots/
+mount -o subvol=timeshift-btrfs/snapshots /dev/nvme0n1p2 /mnt/timeshift-btrfs/snapshots
+
+mount --bind /dev /mnt/dev
+mount --bind /proc /mnt/proc
+mount --bind /sys /mnt/sys
