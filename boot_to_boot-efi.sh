e7a554d2211627c9c4645ca008ffd7c8d10db6f2
RiverMatt <47513427+RiverMatt@users.noreply.github.com>
2024-06-21 11:12:45 -0600

    Simplified ESP mounting

--- boot_to_boot-efi.sh
+++ boot_to_boot-efi.sh
@@ -29,12 +29,13 @@ sed -i 's/\/boot/\/boot\/efi/g' /etc/fstab
 echo "\n\nReloading systemctl daemons to use the updated /etc/fstab..."
 systemctl daemon-reload
 
-echo "\n\nFinding path for ESP device..."
-ESP=$(awk '/\/boot/ {print substr(prev, 3); exit} {prev=$0}' /etc/fstab)
-echo "\nFound ESP device as $ESP"
+#echo "\n\nFinding path for ESP device..."
+#ESP=$(awk '/\/boot/ {print substr(prev, 3); exit} {prev=$0}' /etc/fstab)
+#echo "\nFound ESP device as $ESP"
 
 echo "\n\nMounting ESP device..."
-mount $ESP /boot/efi
+#mount $ESP /boot/efi
+mount -av
 
 echo "\n\nInstalling grub..."
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
