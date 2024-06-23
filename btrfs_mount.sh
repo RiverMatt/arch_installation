3e267a954b92f4613d1bc16ff0da4c955d80091b
RiverMatt <47513427+RiverMatt@users.noreply.github.com>
2024-06-23 14:18:30 -0600

    Update btrfs_mount.sh

--- btrfs_mount.sh
+++ btrfs_mount.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+read -p "Enter the path for the boot device (/dev/nvme0n1p1): " BOOT_PARTITION
+read -p "Enter the mount point for the boot device (/mnt/boot or /mnt/boot/efi): " BOOT_MNT_POINT
+
 mount -o subvol=@ /dev/nvme0n1p2 /mnt
 mount -o subvol=@home /dev/nvme0n1p2 /mnt/home
 mount -o subvol=@log /dev/nvme0n1p2 /mnt/var/log
 mount -o subvol=@pkg /dev/nvme0n1p2 /mnt/var/cache/pacman/pkg
 mount -o subvol=@.snapshots /dev/nvme0n1p2 /mnt/.snapshots
 
-mount /dev/nvme0n1p1 /mnt/boot
+mount $BOOT_PARTITION $BOOT_MNT_POINT
 
 mount --bind /dev /mnt/dev
 mount --bind /proc /mnt/proc
