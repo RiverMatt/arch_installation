66845c3192b90c7d200303c89f62cdf6c07ebd93
Matt <rivermatt@pm.me>
2024-07-09 11:58:35 -0600

    Added a test for pacman init

---
+++ pacman_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Waiting for pacman keyring init to be done"
+
+while ! systemctl show pacman-init.service | grep SubState=exited; do
+    systemctl --no-pager status -n0 pacman-init.service || true
+    sleep 1
+done
