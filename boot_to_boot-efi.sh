236ec37de91ae5a78330997fac97dca1d8e25eb2
RiverMatt <47513427+RiverMatt@users.noreply.github.com>
2024-07-09 11:53:27 -0600

    Added user-definable variables for ESP and kernels

--- boot_to_boot-efi.sh
+++ boot_to_boot-efi.sh
@@ -1,44 +1,58 @@
 #!/bin/bash
 
-echo "\n\nClearing /boot/..."
+read -p "Please enter the desired ESP directory (default /boot/efi/): " ESP_RAW
+if [ -z "$ESP_RAW" ]; then
+    ESP="/boot/efi"
+else
+    ESP=$(echo "$ESP_RAW" | sed 's/^ *//; s/ *$//')
+fi
+
+echo -e "\n\nClearing /boot/..."
 rm -rf /boot/*
 
-echo "\n\nUnmounting /boot/..."
+echo -e "\n\nUnmounting /boot/..."
 umount /boot
 
-echo "\n\nInstalling linux kernel and generating images..."
-pacman -Sy linux
+# Ask if the user wants any additional kernels
+read -p "Please enter any additional linux kernels you would like to install, separated by a space: " KERNELS
+if [ -z "$KERNELS" ]; then
+    echo -e "\n\nInstalling linux kernel and generating images..."
+    pacman -Sy --noconfirm linux linux-headers
+else
+    HEADERS=""
+    for kernel in $KERNELS; do
+        HEADERS+="$kernel-headers "
+    done
+    echo -e "\n\nInstalling linux kernel and $KERNELS and generating images..."
+    pacman -Sy --noconfirm linux linux-headers $KERNELS $HEADERS
+fi
 
-echo "\n\nDetermining system details..."
+echo -e "\n\nDetermining system details..."
 if grep -q 'GenuineIntel' /proc/cpuinfo; then
     echo "The system is running on an Intel processor. Installing intel-ucode..."
-	pacman -Sy intel-ucode
+	pacman -Sy --noconfirm intel-ucode
 elif grep -q 'AuthenticAMD' /proc/cpuinfo; then
     echo "The system is running on an AMD processor. Installing amd-ucode..."
-	pacman -Sy amd-ucode
+	pacman -Sy --noconfirm amd-ucode
 else
     echo "The system's processor is neither Intel nor AMD."
 fi
 
-echo "\n\nMounting /boot/efi..."
-mkdir /boot/efi
+echo -e "\n\nCreating $ESP..."
+mkdir -p $ESP
 
-echo "\n\nEditing /etc/fstab..."
-sed -i 's/\/boot/\/boot\/efi/g' /etc/fstab
+echo -e "\n\nEditing /etc/fstab..."
+ESCAPED_ESP=$(echo "$ESP" | sed 's/\//\\\//g')
+sed -i "s/\/boot/$ESCAPED_ESP/g" /etc/fstab
 
-echo "\n\nReloading systemctl daemons to use the updated /etc/fstab..."
+echo -e "\n\nReloading systemctl daemons to use the updated /etc/fstab..."
 systemctl daemon-reload
 
-#echo "\n\nFinding path for ESP device..."
-#ESP=$(awk '/\/boot/ {print substr(prev, 3); exit} {prev=$0}' /etc/fstab)
-#echo "\nFound ESP device as $ESP"
-
-echo "\n\nMounting ESP device..."
-#mount $ESP /boot/efi
+echo -e "\n\nMounting ESP device..."
 mount -av
 
-echo "\n\nInstalling grub..."
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
+echo -e "\n\nInstalling grub..."
+grub-install --target=x86_64-efi --efi-directory=$ESP --bootloader-id=grub
 
-echo "\n\nCreating grub config..."
+echo -e "\n\nCreating grub config..."
 grub-mkconfig -o /boot/grub/grub.cfg
